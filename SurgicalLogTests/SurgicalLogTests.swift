cd367b4b10fbedaf36b0bd411c48a64929b0e483
ICHIDA.K <zweidrei4c2@icloud.com>
2025-07-16 19:48:48 +0900

    Initial Commit

---
+++ SurgicalLogTests/SurgicalLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  SurgicalLogTests.swift
+//  SurgicalLogTests
+//
+//  Created by Ichida Ko on 2025/07/16.
+//
+
+import Testing
+@testable import SurgicalLog
+
+struct SurgicalLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
